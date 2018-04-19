13ecb9f3674beed20ab2a0f86ea939275ba9c9f3
Derik Gagnon <derik.gagnon@mymail.champlain.edu>
2018-04-19 14:47:10 -0400

    First Commit

---
+++ iOS-ChitChat/MessageCell.swift
@@ -0,0 +1,26 @@
+//
+//  MessageCell.swift
+//  ChitChat
+//
+//  Created by Gagnon, Derik on 4/19/18.
+//  Copyright © 2018 Gagnon, Derik. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell : UITableViewCell {
+ 
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
